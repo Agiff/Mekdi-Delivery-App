d53d07f7e833cf9158499813bdc98fb738ebb8a4
Agiff <ghifarimohammadrahadian@gmail.com>
2023-03-22 00:23:38 +0700

    feat: add flatlist items to menu page

--- client-mobile/screens/MenuScreen.jsx
+++ client-mobile/screens/MenuScreen.jsx
@@ -1,13 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { baseUrl } from '../config';
+import ItemCard from '../components/ItemCard';
 
 export default function MenuScreen() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(baseUrl + 'items')
+      .then(res => {
+        if (!res.ok) throw res.text();
+        return res.json();
+      })
+      .then(data => {
+        setItems(data);
+      })
+      .catch(err => console.log(err));
+  }, [])
+  
+
   return (
-    <SafeAreaView>
-      <Text>MenuScreen</Text>
+    <SafeAreaView style={styles.container}>
+      {
+        <FlatList
+          data={items}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => {
+            return <ItemCard item={item}/>
+          }}
+        />
+      }
     </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
