4a7bb5d8290d98fc10bbedc52fd286eb96a6c5ef
Agiff <ghifarimohammadrahadian@gmail.com>
2023-03-28 01:01:50 +0700

    feat: change mongo and redis url to atlas and lab, add .env files refactor: move orchestrator inside server

--- orchestrator-express/config/index.js
+++
@@ -1,7 +0,0 @@
-const Redis = require('ioredis');
-const redis = new Redis();
-
-const entityUrl = 'http://localhost:4002/';
-const userUrl = 'http://localhost:4001/';
-
-module.exports = { entityUrl, userUrl, redis };
